f63a77b0ef28dde6364fba5c0f407da70735d061
Hristo <infernall92@gmail.com>
2023-12-29 12:56:00 +0200

    Add more content, logic and styles to Header and Quiz components

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,27 @@
-import logoImg from "../assets/quiz-logo.png"
+import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-    return <header>
-        <img src={logoImg} alt="Quiz logo"/>
-        <h1>ReactQuiz</h1>
-    </header>
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => {
+        return [ ...prevAnswers, selectedAnswer]
+    })
+  }
+
+  return (
+    <>
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+            {QUESTIONS[activeQuestionIndex].answers.map(answer => <li key={answer} className="answer">
+                <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>)}
+        </ul>
+      </div>
+    </>
+  );
 }
